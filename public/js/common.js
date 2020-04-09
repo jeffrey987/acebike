f467c7cc05b2e18749d9e16d132b339765d8de93
jeffrey987 <jeffrey_choy@yeah.net>
2020-04-09 17:35:58 +0800

    123

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,4 @@
-$(function () {
-    console.log(22222)
+$(function () {   
     $('body').on('click','#gotop',function(e){
         $('html,body').animate({'scrollTop':0})
     })
@@ -13,9 +12,71 @@ $(function () {
             $('.nav-search').addClass('nav-search-active')
             $('.nav-search').animate({'left':'0','width':'1.2rem'})
         }
-
-
-
     })
+	
+	
+	function displayBarNotification(message, messagetype, timeout) {
+	    var notificationTimeout;
+	
+	    var messages = typeof message === 'string' ? [message] : message;
+	    if (messages.length === 0)
+	        return;
+	
+	    //types: success, error, warning
+	    var cssclass = ['success', 'error', 'warning'].indexOf(messagetype) !== -1 ? messagetype : 'success';
+	
+	    //remove previous CSS classes and notifications
+	    $('#bar-notification')
+	      .removeClass('success')
+	      .removeClass('error')
+	      .removeClass('warning');
+	    $('.modal-dialog').remove();
+	
+	    //add new notifications
+	    var htmlcode = document.createElement('div');
+	    htmlcode.classList.add('modal-dialog', cssclass);
+		
+		
+	    //add close button for notification
+	    var close = document.createElement('span');
+	    close.classList.add('close');
+	    close.setAttribute('title', document.getElementById('bar-notification').dataset.close);
+	
+	    for (var i = 0; i < messages.length; i++) {
+	        var content = document.createElement('p');
+	        content.classList.add('content');
+	        content.innerHTML = messages[i];
+	
+	        htmlcode.append(content);
+	    }
+	    
+	    htmlcode.append(close);
+	
+	    $('#bar-notification')
+	        .append(htmlcode);
+	
+	    $(htmlcode)
+	        .fadeIn('slow')
+	        .on('mouseenter', function() {
+	            clearTimeout(notificationTimeout);
+	        });
+	
+	    //callback for notification removing
+	    var removeNoteItem = function () {
+	        htmlcode.remove();
+	    };
+	
+	    $(close).on('click', function () {
+	        $(htmlcode).fadeOut('slow', removeNoteItem);
+	    });
+	
+	    //timeout (if set)
+	    if (timeout > 0) {
+	        notificationTimeout = setTimeout(function () {
+	            $(htmlcode).fadeOut('slow', removeNoteItem);
+	        }, timeout);
+	    }
+	}
+	
 })
 
